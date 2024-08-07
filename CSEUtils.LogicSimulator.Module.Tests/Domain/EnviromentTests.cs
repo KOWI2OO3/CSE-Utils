d62c2f150868510131f84e22e7fbd72b6ff8b728
Koen Winkel <2003KOWI@gmail.com>
2024-08-07 15:02:39 +0200

    Added logic simulator tests

---
+++ CSEUtils.LogicSimulator.Module.Tests/Domain/EnviromentTests.cs
@@ -0,0 +1,171 @@
+using System.Diagnostics;
+using System.Reflection;
+using CSEUtils.LogicSimulator.Module.Domain;
+using CSEUtils.LogicSimulator.Module.Domain.Gates;
+
+namespace CSEUtils.LogicSimulator.Module.Tests.Domain;
+
+public class EnviromentTests
+{
+
+    [Test]
+    public void AddGateTest() 
+    {
+        var env = new Enviroment();
+        var gate = new LogicGate();
+        Assert.That(env.AddGate(gate), Is.True);
+        Assert.That(GetGates(env)?.ContainsKey(gate.Id), Is.True);
+        Assert.That(GetConnection(env)?.ContainsKey(gate.Id), Is.True);
+    }
+
+    [Test]
+    public void AddGateTwiceTest() 
+    {
+        var env = new Enviroment();
+        var gate = new LogicGate();
+        Assert.That(env.AddGate(gate), Is.True);
+        Assert.That(env.AddGate(gate), Is.False);
+        Assert.That(GetGates(env)?.ContainsKey(gate.Id), Is.True);
+        Assert.That(GetConnection(env)?.ContainsKey(gate.Id), Is.True);
+    }
+
+    [Test]
+    public void RemoveGateTest() 
+    {
+        var env = new Enviroment();
+        var gate = new LogicGate();
+        Assert.That(env.AddGate(gate), Is.True);
+
+        env.RemoveGate(gate.Id);
+        Assert.Multiple(() =>
+        {
+            Assert.That(GetGates(env)?.ContainsKey(gate.Id), Is.False);
+            Assert.That(GetConnection(env)?.ContainsKey(gate.Id), Is.False);
+        });
+    }
+
+    [Test]
+    public void AddConnectionTest() 
+    {
+        var env = new Enviroment();
+        var gate1 = new AndGate();
+        env.AddGate(gate1);
+        
+        var gate2 = new AndGate();
+        env.AddGate(gate2);
+
+        var connection = new Connection(gate1, 0, gate2, 0);
+        env.AddConnection(connection);
+
+        var connections = GetConnection(env);
+        if (connections == null)
+            throw new Exception("Connections dictionary is null");
+
+        Assert.That(connections.ContainsKey(gate1.Id), Is.True);
+        Assert.That(connections.ContainsKey(gate2.Id), Is.True);
+
+        Assert.That(connections[gate1.Id].Item2.Any(c => c.Input.GateId == gate2.Id), Is.True);
+        Assert.That(connections[gate2.Id].Item1.Any(c => c.Output.GateId == gate1.Id), Is.True);
+    }
+
+    [Test]
+    public void RemoveConnectionTest() 
+    {
+        var env = new Enviroment();
+        var gate1 = new AndGate();
+        env.AddGate(gate1);
+        
+        var gate2 = new AndGate();
+        env.AddGate(gate2);
+
+        var connection = new Connection(gate1, 0, gate2, 0);
+        env.AddConnection(connection);
+        
+        env.RemoveConnection(connection);
+
+        var connections = GetConnection(env);
+        if (connections == null)
+            throw new Exception("Connections dictionary is null");
+
+        Assert.That(connections.ContainsKey(gate1.Id), Is.True);
+        Assert.That(connections.ContainsKey(gate2.Id), Is.True);
+
+        Assert.That(connections[gate1.Id].Item2.Any(c => c.Input.GateId == gate2.Id), Is.False);
+        Assert.That(connections[gate2.Id].Item1.Any(c => c.Output.GateId == gate1.Id), Is.False);
+    }
+
+    [Test]
+    public void RemoveGateWithConnectionTest() 
+    {
+        var env = new Enviroment();
+        var gate1 = new AndGate();
+        env.AddGate(gate1);
+        
+        var gate2 = new AndGate();
+        env.AddGate(gate2);
+
+        var connection = new Connection(gate1, 0, gate2, 0);
+        env.AddConnection(connection);
+        
+        env.RemoveGate(gate1.Id);
+
+        var connections = GetConnection(env);
+        if (connections == null)
+            throw new Exception("Connections dictionary is null");
+
+        Assert.That(connections.ContainsKey(gate1.Id), Is.False);
+        Assert.That(connections.ContainsKey(gate2.Id), Is.True);
+
+        Assert.That(connections[gate2.Id].Item1.Any(c => c.Output.GateId == gate1.Id), Is.False);
+    }
+
+    [Test]
+    public void UpdateNonExisitingGateTest() 
+    {
+        var env = new Enviroment();
+        Assert.That(() => InvokeUpdateGate(env, Guid.NewGuid()), Throws.Exception);
+    }
+
+    [Test]
+    public void UpdateGateTest() 
+    {
+        var env = new Enviroment();
+        var notGate = new NotGate();
+        env.AddGate(notGate);
+
+        var connection = new Connection(notGate, 0, notGate, 0);
+        env.AddConnection(connection);
+
+        var result = InvokeUpdateGate(env, notGate.Id);
+        if(result == null)
+            throw new NullReferenceException();
+
+        Assert.That(result!.Count == 1, Is.True);
+        Assert.That(result![0], Is.True);
+
+        // Additional second test to ensure the update takes the cached output value
+        result = InvokeUpdateGate(env, notGate.Id);
+        if(result == null)
+            throw new NullReferenceException();
+        Assert.That(result![0], Is.False);
+    }
+
+    private static Dictionary<Guid, LogicGate>? GetGates(Enviroment env) {
+        var prop = typeof(Enviroment).GetProperty("Gates", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        var getter = prop?.GetGetMethod(nonPublic: true);
+        return getter?.Invoke(env, null) as Dictionary<Guid, LogicGate>;
+    }
+
+    private static Dictionary<Guid, (List<Connection>, List<Connection>)>? GetConnection(Enviroment env) {
+        var prop = typeof(Enviroment).GetProperty("Connections", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        var getter = prop?.GetGetMethod(nonPublic: true);
+        return getter?.Invoke(env, null) as Dictionary<Guid, (List<Connection>, List<Connection>)>;
+    }
+
+    private static List<bool>? InvokeUpdateGate(Enviroment env, Guid gateId) {
+        var method = typeof(Enviroment).GetMethod("UpdateGate", BindingFlags.NonPublic | BindingFlags.Instance);
+        return method?.Invoke(env, [gateId]) as List<bool>;
+    }
+}
