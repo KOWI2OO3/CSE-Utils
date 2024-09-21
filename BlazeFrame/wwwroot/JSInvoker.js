13a2b12f48ee7955c472715e47f2b9d700edc9f3
KOWI2003 <2003kowi@gmail.com>
2024-09-21 09:05:04 +0200

    Fixed formatting in jsInvoker

--- BlazeFrame/wwwroot/JSInvoker.js
+++ BlazeFrame/wwwroot/JSInvoker.js
@@ -78,7 +78,7 @@ export function mouseToCanvasCoordinates(canvas, x, y) {
     return point.matrixTransform(inverseMatrix);
 }
 
-function  getMousePos(canvas, x, y) {
+function getMousePos(canvas, x, y) {
     var rect = canvas.getBoundingClientRect(), // abs. size of element
       scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for x
       scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for y
@@ -87,4 +87,4 @@ function  getMousePos(canvas, x, y) {
       x: (x - rect.left) * scaleX,   // scale mouse coordinates after they have
       y: (y - rect.top) * scaleY     // been adjusted to be relative to element
     }
-  }
+}
